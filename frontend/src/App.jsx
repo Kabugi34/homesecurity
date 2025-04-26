59d02397e1e92ffc5df5a2ac12b961d8674d7e61
boogie <kabugifrancis34@gmail.com>
2025-04-26 03:41:41 +0300

    securing API keys for firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes, Route} from "react-router-dom";
+import { BrowserRouter , Routes, Route} from "react-router-dom";
 import Landing from "./pages/Landing";  
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -12,7 +12,6 @@ import Notifications from "./pages/dashboard/Notifications";
 
 function App() {
   return (
-    
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/register" element={<Register />} />
--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -1,19 +1,18 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3QmjtMr-mQt6LEg4ctQPQvVVeshx6guU",
-  authDomain: "home-security-9d841.firebaseapp.com",
-  projectId: "home-security-9d841",
-  storageBucket: "home-security-9d841.firebasestorage.app",
-  messagingSenderId: "147263415352",
-  appId: "1:147263415352:web:45c3db92050fb65e6572cd",
-  measurementId: "G-JJ5M10FNCB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export  const auth = getAuth(app);
+const auth = getAuth(app);
+
+export { auth };
