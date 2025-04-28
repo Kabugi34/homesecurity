c73c6ca5b20f353d29c71ba820bcf279e69438b6
boogie <kabugifrancis34@gmail.com>
2025-04-29 00:46:24 +0300

    added a form to add a persons name and images

--- frontend/src/pages/dashboard/AddPerson.jsx
+++ frontend/src/pages/dashboard/AddPerson.jsx
@@ -1,10 +1,72 @@
-const AddPerson = () => {
+import React ,{useState} from 'react';
+import axios from 'axios';
+const AddPerson =() =>{
+  const [name,setName] =useState('');
+  const[images ,setImages] =useState([]);
+  const[uploading,setUploading]=useState(false);
+  const [message, setMessage] = useState('');
+
+  const handleSubmit=async(e) =>{
+    e.preventDefault();
+    if (!name || images.length ===0){
+      setMessage('provide a name and at least four images')
+      return;
+    }
+    const formData =new FormData();
+    formData.append('name',name);
+    for (const image of images) {
+      formData.append('images', image);
+    }
+    try {
+      setUploading(true);
+      const response =await axios.post('http:localhost:8000/add_person',formData,{
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setMessage(response.data.message||'Person added successfully!');
+      setName('');
+      setImages([]);
+      
+    }catch (error){
+      console.error('Error details:', error);
+      setMessage('Error adding person. Please try again.');
+
+    }finally {
+      setUploading(false);
+    }
+  
+  };
   return (
-    <div className="text-2xl font-semibold text-gray-800">
-      Add Person
-      <p className="text-base mt-2 text-gray-600">Upload images of a new known person and retrain model.</p>
+    <div className ="p-6">
+      <h2 className ="text-3x1 font-bold mb-6">Add a new person</h2>
+      <form onSubmit ={handleSubmit} className="flex flex-col gap-4  w-full max-w-md">
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter name"
+          className="p-2 border border-gray-300 rounded"
+          required
+        />
+        <input 
+          type="file"
+          accept="image/*"
+          onChange={(e) => setImages(Array.from(e.target.files))}
+          multiple
+          className="p-2 border border-gray-300 rounded"
+          required
+        />
+        <button
+          type ="submit"
+          disabled={uploading}
+          className ="px-6 py-3 bg-green-600 text-white rounded hover:bg-gray-700 disabled:opacity-25"
+        >
+          {uploading ? 'Uploading...' : 'Add Person'}
+        </button>
+        {message && <p className="text-center text-sm text-gray-700 mt-2">{message}</p>}
+      </form>
     </div>
   );
 };
-
 export default AddPerson;
